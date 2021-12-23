671faf51ac20466885536e872875141a29814e6c
khanhpt-2853 <pham.tan.khanh@sun-asterisk.com>
2021-12-23 15:19:54 +0700

    Chapter 9: Advance Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,43 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save :downcase_email
 
-  validates :name, presence: true, length: {minimum: Settings.length.min_10}
+  validates :name, presence: true, length: {minimum: Settings.length.digit_10}
   validates :email, presence: true, uniqueness: true,
-            length: {maximum: Settings.length.max_255},
+            length: {maximum: Settings.length.digit_255},
             format: {with: Settings.regex.email}
-  validates :password, length: {minimum: Settings.length.min_10}
+  validates :password, length: {minimum: Settings.length.digit_10}
   has_secure_password
 
   class << self
     def digest string
       cost = if ActiveModel::SecurePassword.min_cost
-              BCrypt::Engine::MIN_COST
-            else
-              BCrypt::Engine.cost
-            end
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
       BCrypt::Password.create string, cost: cost
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def forget
+    update_column :remember_digest, nil
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
 
   private
 
