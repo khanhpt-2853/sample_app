71290a6e67cbd651f46a229304e4859c5042eb9c
khanhpt-2853 <pham.tan.khanh@sun-asterisk.com>
2021-12-24 14:54:25 +0700

    Chapter 11: Account Activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = t ".success"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = t ".please_check_mail"
+      redirect_to root_url
     else
       flash.now[:danger] = t ".failure"
       render :new
